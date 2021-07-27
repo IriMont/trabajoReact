9a51e6fbed2e35d98965d97047bd793805f3b628
Irina Montoya <82009613+IriMont@users.noreply.github.com>
2021-07-26 21:17:02 -0500

    NavBar/IrinaMontoya

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+import Header from "./header"
+import NavBar from "./NavBar"
+
+const App = () => 
+    <>
+        <Header/>
+        <NavBar/>
+    </>
+
+export default App
---
+++ src/Components/NavBar.js
@@ -0,0 +1,7 @@
+const NavBar = () =>
+    <nav>
+        <a href="#">Productos</a>
+        <a href="#">Ofertas</a>
+        <a href="#">Canasta de compra</a>
+    </nav>
+export default NavBar
