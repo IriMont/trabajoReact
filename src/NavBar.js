563f1dd2728e212394397373cf3fe0597331da40
Irina Montoya <82009613+IriMont@users.noreply.github.com>
2021-07-26 21:00:56 -0500

    second commit

---
+++ src/NavBar.js
@@ -0,0 +1,7 @@
+const NavBar = () =>
+    <nav>
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+    </nav>
+export default NavBar
---
+++ src/header.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar"
+
+const Header = () =>
+    <header>
+        <h1>Web ecommerce</h1>
+        <NavBar/>
+    </header>
+
+export default Header
